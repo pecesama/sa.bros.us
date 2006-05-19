077b919032bd06de3e887d71994c6af7e7303744
kurtmckee <kurtmckee@7d817b1d-ea13-0410-87bb-8e4317e5ca23>
2006-05-19 21:45:41 +0000

    Separate HTML and PHP code as much as possible.

--- rss.php
+++ rss.php
@@ -7,86 +7,84 @@
   sabrosus is a free software licensed under GPL (General public license)
 
   =========================== */
-?>
-<?php
-	include("include/config.php");
-	include("include/conex.php");		
-	include("include/functions.php");
-	include("lang/".$Sabrosus->archivoIdioma);
 
-	header("Content-type: text/xml; charset=utf-8");
-	echo "<?xml version=\"1.0\""." encoding=\"".$idioma[codificacion]."\"?>\n";
+include("include/config.php");
+include("include/conex.php");
+include("include/functions.php");
+include("lang/".$Sabrosus->archivoIdioma);
 
- 	/* PATCH Bug #1242025 */
-	if (isset($_GET["tag"]))
-	{
-		$navegador = strtolower( $_SERVER['HTTP_USER_AGENT'] );
-		if (stristr($navegador, "opera") || stristr($navegador, "msie"))
-			/* PATCH Bug #1359231 */
-    		$tagtag=$_GET["tag"];
-			if (isset($tagtag)) {
-    			$tagtag = utf8_decode($_GET["tag"]);
-    		}
-    		/* End PATCH Bug #1359231 */
-		else
-			$tagtag = $_GET["tag"];
-	}
-	/* End PATCH Bug #1242025 */
+header("Content-type: text/xml; charset=utf-8");
+echo "<?xml version=\"1.0\""." encoding=\"".$idioma[codificacion]."\"?>\n";
 
-	if(isset($cuantos))
-	{
-		//Parametro $cuantos=todos para que devuelvas una sindicación completa de Sabrosus.
-		if($cuantos=='todos')
-			$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC " : "SELECT * FROM ".$prefix."sabrosus WHERE tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC");		
-		else
-			$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC limit $cuantos" : "SELECT * FROM ".$prefix."sabrosus where tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC limit $cuantos");
+if (isset($_GET["tag"])) {
+	$navegador = strtolower($_SERVER['HTTP_USER_AGENT']);
+	if (stristr($navegador, "opera") || stristr($navegador, "msie")) {
+		$tagtag = utf8_decode($_GET["tag"]);
 	} else {
-		$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC limit 10" : "SELECT * FROM ".$prefix."sabrosus where tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC limit 10");
+		$tagtag = $_GET["tag"];
 	}
-	$result = mysql_query($sqlStr,$link);	
+}
 
-	echo "<rss version=\"2.0\">\n";
-	echo "	<channel>\n";
-	echo "		<title>sa.bros.us/".$Sabrosus->siteName."</title>\n";
-	echo "		<link>".$Sabrosus->sabrUrl."</link>\n";
-	echo "		<description>".$idioma[enlaces_de]." ".$Sabrosus->siteName."</description>\n";
-	echo "		<language>".$idioma[nombre_estandar]."</language>\n";
-	echo "		<docs>http://blogs.law.harvard.edu/tech/rss</docs>\n";
-	echo "		<generator>Sa.bros.us v".version()."</generator>\n";
-	echo "		<image>\n";
-	echo "			<url>".$Sabrosus->sabrUrl."/images/sabrosus_icon.png</url>\n";
-	echo "			<title>".$idioma[enlaces_de]." ".$Sabrosus->siteName."</title>\n";
-	echo "			<link>".$Sabrosus->sabrUrl."</link>\n";
-	echo "		</image>\n";
-	while ($registro = mysql_fetch_array($result))
-	{
-		$titulo=limpiaHTML($registro["title"]);			
-		$desc=limpiaHTML($registro["descripcion"]);
-		$tags=limpiaHTML($registro["tags"]);
-		$url=limpiaHTML($registro["enlace"]);
-		
-		$fecha = gmdate("D, d M Y H:i:s \G\M\T", strtotime($registro["fecha"]));
-		
-		/* Control de Enlaces Privados */
-		$privado=false;
-		$etiquetas = explode(" ",$tags);
-		foreach($etiquetas as $etiqueta){
-			if ($etiqueta==":sab:privado") { 
-				$privado=true;
+if (isset($cuantos)) {
+	//Parametro $cuantos=todos para que devuelvas una sindicación completa de Sabrosus.
+	if($cuantos=='todos')
+		$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC " : "SELECT * FROM ".$prefix."sabrosus WHERE tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC");
+	else
+		$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC limit $cuantos" : "SELECT * FROM ".$prefix."sabrosus where tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC limit $cuantos");
+} else {
+	$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC limit 10" : "SELECT * FROM ".$prefix."sabrosus where tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC limit 10");
+}
+$result = mysql_query($sqlStr,$link);
+
+?>
+
+<rss version="2.0">
+	<channel>
+		<title>sa.bros.us/<?=$Sabrosus->siteName;?></title>
+		<link><?=$Sabrosus->sabrUrl;?></link>
+		<description><?=$idioma[enlaces_de]." ".$Sabrosus->siteName;?></description>
+		<language><?=$idioma[nombre_estandar];?></language>
+		<docs>http://blogs.law.harvard.edu/tech/rss</docs>
+		<generator>Sa.bros.us <?=version();?></generator>
+		<image>
+			<url><?=$Sabrosus->sabrUrl;?>/images/sabrosus_icon.png</url>
+			<title><?=$idioma[enlaces_de]." ".$Sabrosus->siteName;?></title>
+			<link><?=$Sabrosus->sabrUrl;?></link>
+		</image>
+
+		<?
+		while ($registro = mysql_fetch_array($result)) {
+			/* Control de Enlaces Privados */
+			$tags=limpiaHTML($registro["tags"]);
+			$privado=false;
+			$etiquetas = explode(" ", $tags);
+			foreach ($etiquetas as $etiqueta) {
+				if ($etiqueta==":sab:privado") {
+					$privado=true;
+				}
+			}
+
+			if (!$privado) {
+				$titulo = limpiaHTML($registro["title"]);
+				$desc = limpiaHTML($registro["descripcion"]);
+				$url = limpiaHTML($registro["enlace"]);
+				$fecha = gmdate("D, d M Y H:i:s \G\M\T", strtotime($registro["fecha"]));
+
+				?>
+
+				<item>
+					<title><?=$titulo;?></title>
+					<link><?=$url;?></link>
+					<description><?=$desc;?></description>
+					<pubDate><?=$fecha;?></pubDate>
+					<category><?=$tags;?></category>
+					<guid isPermaLink="true"><?=$Sabrosus->sabrUrl."/ir.php?id=".$registro['id_enlace'];?></guid>
+				</item>
+
+				<?
 			}
 		}
-		if(!$privado)
-		{
-			echo "		  <item>\n";
-			echo "			 <title>".$titulo."</title>\n";
-			echo "		     <link>".$url."</link>\n";
-			echo "		     <description>".$desc."</description>\n";
-			echo "		     <pubDate>".$fecha."</pubDate>\n";
-			echo "			 <category>".$tags."</category>\n";
-			echo "           <guid isPermaLink=\"true\">".$Sabrosus->sabrUrl . "/ir.php?id=" . $registro['id_enlace'] ."</guid>";
-			echo "		  </item>\n";
-		}
-	}
-	echo "	</channel>";
-	echo "</rss>";
-?>
+		?>
+
+	</channel>
+</rss>
