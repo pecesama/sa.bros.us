39e0acc3523029551229e2afe602cc998677e800
saboru <saboru@7d817b1d-ea13-0410-87bb-8e4317e5ca23>
2006-05-19 16:14:11 +0000

    Solucionados los problemas con el importador de delicious

--- include/importdelicious.php
+++ include/importdelicious.php
@@ -1,23 +1,24 @@
 <?php
 
-	include_once("include/config.php");
-	include_once("include/conex.php");	
-	include_once("include/functions.php");
 	include_once("include/parsing.php");
 	include_once("include/get.php");
 
 	function importdelicious($user,$pass)
 	{
 		global $prefix;
+		global $link;
+		
 		$xml="http://del.icio.us/api/posts/all?";
 		$get=new Get();
 		$src_obj=$get->getContent($xml,$user,$pass);
 		$src=$src_obj->response();
+		
 		$urlAttrib="href";
 		$titleAttrib="description";
 		$descAttrib="extended";
 		$tagAttrib="tag";
 		$timeAttrib="time";
+		
 		if ($src_obj->responseCode()==200)
 		{  
 			$posts=getSubTree("posts",$src,0);
@@ -30,28 +31,29 @@
 			while (true)
 			{
 				$p++;
-				$post=getTag("post",$posts);
-				$url=getAttrib($post,$urlAttrib);
-				$title=getAttrib($post,$titleAttrib);
-				$description=utf8_decode(getAttrib($post,$descAttrib));
-				$tags=getAttrib($post,$tagAttrib);
-				$time=getAttrib($post,$timeAttrib);
+				$post = getTag("post",$posts);
+				$url = getAttrib($post,$urlAttrib);
+				$title = getAttrib($post,$titleAttrib);
+				$description = utf8_decode(getAttrib($post,$descAttrib));
+				$tags = getAttrib($post,$tagAttrib);
+				$time = getAttrib($post,$timeAttrib);
 				
 				$bookmark=array();
-				$bookmark["title"]=utf8_decode($title);
-				$bookmark["tags"]=$tags; /* Aqui hay que solucionar el Feature #1436022 */
-				$bookmark["enlace"]=$url;
-				$bookmark["descripcion"]=$description;
-				$bookmark["fecha"] = date("Y-m-d H:i:s"); /* Aqui tambien el Feature #1436022 */
-				$Sql="select enlace from ".$prefix."sabrosus where enlace='".$bookmark["enlace"]."'";
-				$result = mysql_query($Sql,$link);
-				if(mysql_num_rows($result)==0)
+				$bookmark["title"] = trim($title);
+				$bookmark["tags"] = $tags;
+				$bookmark["enlace"] = $url;
+				$bookmark["descripcion"] = $description;
+				$bookmark["fecha"] = date("Y-m-d H:i:s",strtotime($time));
+				if ($bookmark["enlace"])
 				{
-					$Sql="insert into ".$prefix."sabrosus (title,tags,enlace,descripcion,fecha)  values ('".$bookmark["title"]."','".$bookmark["tags"]."','".$bookmark["enlace"]."','".$bookmark["descripcion"]."', '".$bookmark["fecha"]."');";
-					mysql_query($Sql,$link);
-					$imported++;
+					if(!isInSabrosus($bookmark["enlace"]))
+					{
+						$Sql="insert into ".$prefix."sabrosus (title,tags,enlace,descripcion,fecha)  values ('".$bookmark["title"]."','".$bookmark["tags"]."','".$bookmark["enlace"]."','".$bookmark["descripcion"]."', '".$bookmark["fecha"]."');";
+						mysql_query($Sql,$link);
+						$imported++;
+					}
+					$total++;
 				}
-				$total++;
 				$p=findnext("post",$posts,$p);
 				if (!$p)
 				{
@@ -59,7 +61,7 @@
 				}
 				$posts=substr($posts,$p+1);
 			}
-			return array("responseCode"=>$src_obj->responseCode(),"total"=>$total,"imported"=>$imported-2);
+			return array("responseCode"=>$src_obj->responseCode(),"total"=>$total,"imported"=>$imported);
 		} else {
 			return array("responseCode"=>$src_obj->responseCode());
 		}
