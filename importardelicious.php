405fed6da227b93954e1cfe81526869797242c38
saboru <vbracco@gmail.com>
2006-05-20 20:21:52 +0000

    Solucion de problemas

--- importardelicious.php
+++ importardelicious.php
@@ -7,7 +7,8 @@
   sabrosus is a free software licensed under GPL (General public license)
 
   =========================== */
-
+?>
+<?
 	include("include/config.php");
 	include("include/conex.php");
 	include("include/functions.php");
@@ -15,18 +16,23 @@
 
 if (esAdmin())
 {
-	$accion=$_POST["accion"];
-	if ($accion=="import") {
+        if (isset($_POST["username"])){
+          $isImporting=true;
+        }else{
+          $isImporting=false;
+        }
+        echo "Importando: ".$isImporting;
+	if ($isImporting) {
 		include "include/importdelicious.php";
-		$respImport=importdelicious($_POST["username"],$_POST["password"]);
+        	$respImport=importdelicious($_POST["username"],$_POST["password"]);
 	} 
 ?>
-<!-- Sa.bros.us monousuario version <?=version();?> -->
+<!-- Sa.bros.us monousuario version 1.7 -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=$idioma[nombre_estandar]?>" lang="<?=$idioma[nombre_estandar]?>">
 <head>
 	<title><?=$idioma[deli_titulo]?>/sa.bros.us</title>
-	<meta name="generator" content="Sa.bros.us <?=version();?>" />
+	<meta name="generator" content="Sa.bros.us 1.7" />
 	<meta http-equiv="Content-Type" content="text/html; charset=<?=$idioma[codificacion]?>" />
 	<link rel="stylesheet" href="<?=$Sabrosus->sabrUrl?>/sabor.css" type="text/css" />
 	<link rel="shortcut icon" href="<?=$Sabrosus->sabrUrl?>/images/sabrosus_icon.png" />
@@ -42,7 +48,7 @@ if (esAdmin())
 	</div>
 	<div id="contenido">
 <?
-	if ($accion=="import")
+	if ($isImporting)
 	{
 		if ($respImport["responseCode"]=="200")
 		{
@@ -82,8 +88,7 @@ if (esAdmin())
 					<input type="text" name="username" /><br />	
 					<label for="password"><?=$idioma[deli_pass];?></label><br />
 					<input type="password" name="password" /><br />
-					<input class="no_style" type="hidden" name="accion" value="import" />
-					<input class="submit" type="submit" name="btnsubmit" value="<?=$idioma[deli_boton];?>" />
+					<input class="submit" type="submit" name="accion" value="<?=$idioma[deli_boton];?>" />
 				</fieldset>
 			</form>	
 		</div>
