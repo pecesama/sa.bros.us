0552740836dccc1636998de278007acd5dbbd79b
kurtmckee <kurtmckee@gmail.com>
2006-05-21 19:17:30 +0000

    Fix tag removal logic; simplify code

--- include/addtags.js
+++ include/addtags.js
@@ -1,40 +1,28 @@
-function addTag(tagText)
-{
-	var repetida=false;
+function addTag(tagText) {
+	var repetida = false;
+	var tags = new Array();
+	var etiquetas = new String();
+	var temp = new String();
+
 	etiquetas = document.getElementById('etiquetas');
-	tags=etiquetas.value.split(' ');
-	for (var i=0; i<=tags.length; i++) {
+	tags = etiquetas.value.split(' ');
+
+	for (var i=0; i<tags.length; i++) {
 		if (tags[i] == tagText) {
-			//return false;
-			repetida=true;
+			tags[i] = "";
+			repetida = true;
 		}
 	}
-	if (!etiquetas.value) {
-		etiquetas.value = tagText;
+	if (!repetida) {
+		tags[tags.length] = tagText;
 	}
-	else if (etiquetas.value && !repetida) {
-		etiquetas.value += ' ' + tagText;
-	} else {
-		etiquetas.value = removeSubstring(etiquetas.value, tagText);
-		etiquetas.value = trimAll(etiquetas.value.replace('  ', ' '));
+
+	temp = "";
+	for (var i=0; i<tags.length; i++) {
+		if (tags[i] != '') {
+			temp = temp + " " + tags[i];
+		}
 	}
+	etiquetas.value = temp.substring(1);
 	return true;
-}
-
-function removeSubstring(s, t) {
-  i = s.indexOf(t);
-  r = "";
-  if (i == -1) return s;
-  r += s.substring(0,i) + removeSubstring(s.substring(i + t.length), t);
-  return r;
-}
-
-function trimAll(sString) {
-	while (sString.substring(0,1) == ' ') {
-		sString = sString.substring(1, sString.length);
-	}
-	while (sString.substring(sString.length-1, sString.length) == ' ') {
-		sString = sString.substring(0,sString.length-1);
-	}
-	return sString;
 }
