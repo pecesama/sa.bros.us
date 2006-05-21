d7ab8b012b7df1151e21449ccdf33f08dfcda5ba
kurtmckee <kurtmckee@gmail.com>
2006-05-21 21:02:51 +0000

    Prepare to merge agregar.php and modifica.php

--- modifica.php
+++ modifica.php
@@ -8,25 +8,25 @@
 
   =========================== */
 
-	include("include/config.php");
-	include("include/conex.php");	
-	include("include/functions.php");
-	include("lang/".$Sabrosus->archivoIdioma);
-	
-	if($_SERVER["REQUEST_METHOD"]=="POST") {
-		if (!esAdmin()) {
-			header("Location: login.php");
-		} else {
-			if (isset($_POST["privado"])) {
-				$etiquetas = ":sab:privado ".$_POST["etiquetas"];
-			} else {
-				$etiquetas = $_POST["etiquetas"];
-			}
-			$Sql="UPDATE ".$prefix."sabrosus SET title='".$_POST["title"]."', tags='".$etiquetas."', enlace='".$_POST["enlace"]."', descripcion='".$_POST["descripcion"]."' where id_enlace=".$_POST["id_enlace"];
-			mysql_query($Sql,$link);
-			header("Location: cpanel.php");
-		}
+include("include/config.php");
+include("include/conex.php");
+include("include/functions.php");
+include("lang/".$Sabrosus->archivoIdioma);
+
+if (!esAdmin()) {
+	header("Location: login.php");
+}
+
+if($_SERVER["REQUEST_METHOD"]=="POST") {
+	if (isset($_POST["privado"])) {
+		$etiquetas = ":sab:privado ".$_POST["etiquetas"];
 	} else {
-		header("Location: index.php");
+		$etiquetas = $_POST["etiquetas"];
 	}
+	$Sql="UPDATE ".$prefix."sabrosus SET title='".$_POST["title"]."', tags='".$etiquetas."', enlace='".$_POST["enlace"]."', descripcion='".$_POST["descripcion"]."' where id_enlace=".$_POST["id_enlace"];
+	mysql_query($Sql,$link);
+	header("Location: cpanel.php");
+} else {
+	header("Location: index.php");
+}
 ?>
