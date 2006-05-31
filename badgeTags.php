6600ae3fcc3fb493ef52b07361730c5130f830c6
ravenlp <ravenlp@7d817b1d-ea13-0410-87bb-8e4317e5ca23>
2006-05-31 17:51:40 +0000

    Agregado lo que se sugerio en el foro y cabiado el orden de algunas cosas

--- badgeTags.php
+++ badgeTags.php
@@ -51,9 +51,9 @@ echo "document.write(\"<div id='badgetags'> \");\n";
 		}
 		$i = 0;
 		echo "document.write(\"<ol>\");\n";
-		foreach($keys as $key){
 		
-					/*  color tags */
+		
+			/*  color tags */
 			$colores[0][r] = 0.1;
 			$colores[0][g] = 2;
 			$colores[0][b] = 4;
@@ -73,6 +73,10 @@ echo "document.write(\"<div id='badgetags'> \");\n";
 			$colores[4][r] = 1.5;
 			$colores[4][g] = 1.5;
 			$colores[4][b] = 1.5;
+			
+				foreach($keys as $key){
+		
+
 		
 			$colores[5][r] = (rand(5,40)) / 10;
 			$colores[5][g] = (rand(5,40)) / 10;
