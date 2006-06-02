44798cc3f73b1c2dad5fe60e30a03f3d702e01d1
kurtmckee <kurtmckee@gmail.com>
2006-06-02 00:47:00 +0000

    No longer rely on tmp/ to output lastExport.html

--- exportar.php
+++ exportar.php
@@ -8,66 +8,47 @@
 
   =========================== */
 
-	include("include/config.php");
-	include("include/conex.php");
-	include("include/functions.php");
-	include("lang/".$Sabrosus->archivoIdioma);
+include("include/config.php");
+include("include/conex.php");
+include("include/functions.php");
+include("lang/".$Sabrosus->archivoIdioma);
 
 
-	$toFile = '<!DOCTYPE NETSCAPE-Bookmark-file-1>
-				<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-				<TITLE>Bookmarks</TITLE>
-				<H1>Bookmarks</H1>
+$toFile = '
+<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Bookmarks</TITLE>
+<H1>Bookmarks</H1>
 
-				<DL><p>
-   					<DT><H3>Enlaces de Sa.Bros.us</H3>
-    				<DL><p>
-					';
-		
-	$sqlStr = 'SELECT title, enlace FROM '.$prefix.'sabrosus  ';
-		
-	if(!isset($_POST['links_sel']) || (count($_POST['links_sel']) == 0)){
-		if (!esAdmin()) {
-			header("Location: login.php");
-		}
-	}else{
-		$link_id = $_POST['links_sel'];
-		$sqlStr .=	'WHERE id_enlace = '.$link_id[0];
-		for($i = 1; $i < count($link_id); $i++){
-			$sqlStr .= ' OR id_enlace ='.$link_id[$i];
-		}
-	}
+<DL><p>
+	<DT><H3>Enlaces de Sa.Bros.us</H3>
+	<DL><p>
+';
 	
+$sqlStr = 'SELECT title, enlace FROM '.$prefix.'sabrosus  ';
 	
-	$result = mysql_query($sqlStr);
-	while ($row = mysql_fetch_array($result)){
-		$toFile .= '<DT><A HREF="'.$row['enlace'].'" ADD_DATE="'.date("U").'" LAST_CHARSET="UTF-8">'.$row['title'].'</A>';
+if(!isset($_POST['links_sel']) || (count($_POST['links_sel']) == 0)){
+	if (!esAdmin()) {
+		header("Location: login.php");
 	}
-	$toFile .= '    </DL><p>
-				</DL><p>';
-				
-	$archivoSalida = "lastExport.html";
-	if (!is_writeable("tmp/".$archivoSalida)) {
-		$_SESSION['error_exporting'] = '1';
-		if(!isset($_POST['links_sel']) || (count($_POST['links_sel']) == 0)){
-			header("Location: cpanel.php?er=1");
-		}else{
-			header("Location: index.php?er=1");
-		}
+}else{
+	$link_id = $_POST['links_sel'];
+	$sqlStr .=	'WHERE id_enlace = '.$link_id[0];
+	for($i = 1; $i < count($link_id); $i++){
+		$sqlStr .= ' OR id_enlace ='.$link_id[$i];
 	}
-	if($fp = fopen('tmp/'.$archivoSalida,"w+")){
-		fwrite($fp, $toFile);
-		fclose($fp);
-		header ("Content-Disposition: attachment; filename=\"".$archivoSalida."\"");
-		header ("Content-Type: application/octet-stream");
-		header ("Content-Length: ".filesize('tmp/'.$archivoSalida));
-		$fp = fopen('tmp/'.$archivoSalida,"r");
-		fpassthru($fp);
-	}else{
-		if(!isset($_POST['links_sel']) || (count($_POST['links_sel']) == 0)){
-			header("Location: cpanel.php");
-		}else{
-			header("Location: index.php");
-		}
-	}
+}
+
+
+$result = mysql_query($sqlStr);
+while ($row = mysql_fetch_array($result)){
+	$toFile .= '<DT><A HREF="'.$row['enlace'].'" ADD_DATE="'.date("U").'" LAST_CHARSET="UTF-8">'.$row['title'].'</A>';
+}
+$toFile .= '</DL><p></DL><p>';
+			
+header ("Content-Disposition: attachment; filename=\"lastExport.html\"");
+header ("Content-Type: text/html");
+header ("Content-Length: ".strlen($toFile));
+echo $toFile;
+
 ?>
