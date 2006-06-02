32384a0f25ee759e01e6a6c87936a993b5a8df1f
kurtmckee <kurtmckee@7d817b1d-ea13-0410-87bb-8e4317e5ca23>
2006-06-02 06:13:54 +0000

    Security fixes (exit after HTTP redirect). Normalize tags' whitespace.

--- modifica.php
+++ modifica.php
@@ -15,13 +15,13 @@ include("lang/".$Sabrosus->archivoIdioma);
 
 if (!esAdmin()) {
 	header("Location: login.php");
+	exit();
 }
 
 if($_SERVER["REQUEST_METHOD"]=="POST") {
+	$etiquetas = normalizeTags($_POST["etiquetas"]);
 	if (isset($_POST["privado"])) {
-		$etiquetas = ":sab:privado ".$_POST["etiquetas"];
-	} else {
-		$etiquetas = $_POST["etiquetas"];
+		$etiquetas = ":sab:privado ".$etiquetas;
 	}
 	$Sql="UPDATE ".$prefix."sabrosus SET title='".$_POST["title"]."', tags='".$etiquetas."', enlace='".$_POST["enlace"]."', descripcion='".$_POST["descripcion"]."' where id_enlace=".$_POST["id_enlace"];
 	mysql_query($Sql,$link);
