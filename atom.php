847f6fdb19aa3b4ca492d636b4a8659fbb67232b
saboru <saboru@7d817b1d-ea13-0410-87bb-8e4317e5ca23>
2006-10-25 18:48:42 +0000

    Optimizacion de consultas

--- atom.php
+++ atom.php
@@ -9,10 +9,13 @@
   =========================== */
 
 $feeds = true;
+header("Content-type: text/xml; charset=utf-8");
+
 include("include/config.php");
 include("include/conex.php");
 include("include/functions.php");
 
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 if (isset($_GET["tag"])) {
 	$navegador = strtolower( $_SERVER['HTTP_USER_AGENT'] );
 	if (stristr($navegador, "opera") || stristr($navegador, "msie")) {
@@ -22,26 +25,29 @@ if (isset($_GET["tag"])) {
 	}
 }
 
-if(isset($cuantos)) {
-	//Parametro $cuantos=todos para que devuelvas una sindicación completa de sabros.us.
-	if($cuantos=='todos') {
-		$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC " : "SELECT * FROM ".$prefix."sabrosus WHERE tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC");
-	} else {
-		$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC limit $cuantos" : "SELECT * FROM ".$prefix."sabrosus where tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC limit $cuantos");
+$sqlStr = "SELECT * FROM ".$prefix."sabrosus WHERE";
+if(isset($tagtag)){
+	$sqlStr .= " ((tags NOT LIKE '%:sab:privado%')";
+	$sqlStr .= " AND (tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag'))";
+} else {
+	$sqlStr .= " (tags NOT LIKE '%:sab:privado%')";
+}
+$sqlStr .= " ORDER BY fecha DESC";
+if(isset($cuantos)){
+	if($cuantos!='todos' && is_numeric($cuantos)){
+		$sqlStr .= " LIMIT $cuantos";
+	}
+	if($cuantos!='todos' && !is_numeric($cuantos)){
+		$sqlStr .= " LIMIT 10";
 	}
 } else {
-	$sqlStr = (!isset($tagtag) ? "SELECT * FROM ".$prefix."sabrosus ORDER BY fecha DESC limit 10" : "SELECT * FROM ".$prefix."sabrosus where tags LIKE '% $tagtag %' OR tags LIKE '$tagtag %' OR tags LIKE '% $tagtag' OR tags = '$tagtag' ORDER BY fecha DESC limit 10");
+	$sqlStr .= " LIMIT 10";
 }
-
 $result = mysql_query($sqlStr,$link);
-
-header("Content-type: text/xml; charset=utf-8");
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-
 ?>
 
 <feed xmlns="http://www.w3.org/2005/Atom" xml:lang="<?=strtolower(str_replace("_","-",$locale));?>">
-	<title><?=$Sabrosus->siteName;?></title>
+	<title>sabros.us/<?=$Sabrosus->siteName;?></title>
 	<author><name><?=$Sabrosus->siteName;?></name></author>
 	<id><?=$Sabrosus->sabrUrl;?></id>
 	<updated><?=gmdate("Y-m-d\TH:i:s\Z");?></updated>
@@ -59,50 +65,34 @@ while ($registro = mysql_fetch_array($result)) {
 	$url=htmlspecialchars($registro["enlace"]);
 
 	$fecha = gmdate("Y-m-d\TH:i:s\Z", strtotime($registro["fecha"]));
+	?>
+	<entry>
+		<title><?=$titulo;?></title>
+		<link rel="alternate" href="<?=$Sabrosus->sabrUrl.chequearURLFriendly("/","/ir.php?id=").$registro["id_enlace"];?>" title="<?=$titulo." @ ".$Sabrosus->siteName;?>" />
+		<link rel="related" href="<?=$url;?>" title="<?=$titulo;?>" />
 
-	/* Control de Enlaces Privados */
-	$privado=false;
-	$etiquetas = explode(" ",$tags);
-	foreach($etiquetas as $etiqueta) {
-		if ($etiqueta==":sab:privado") {
-			$privado=true;
-		}
-	}
+		<id><?=strtolower($Sabrosus->sabrUrl."/".$registro["id_enlace"]);?></id>
 
-	if (!$privado) {
-		?>
-
-		<entry>
-			<title><?=$titulo;?></title>
-			<link rel="alternate" href="<?=$Sabrosus->sabrUrl.chequearURLFriendly("/","/ir.php?id=").$registro["id_enlace"];?>" title="<?=$titulo." @ ".$Sabrosus->siteName;?>" />
-			<link rel="related" href="<?=$url;?>" title="<?=$titulo;?>" />
-
-			<id><?=strtolower($Sabrosus->sabrUrl."/".$registro["id_enlace"]);?></id>
-
-			<content type="html">
-				<?=htmlspecialchars("<div><a href=\"".$url."\">".$titulo."</a></div>");?>
-				<? if ($desc) { ?>
-					<?=htmlspecialchars("<div>".$desc."</div>");?>
-				<? } ?>
-			</content>
-
-			<updated><?=$fecha;?></updated>
-
-			<?
-			foreach($etiquetas as $etiqueta) {
-				if ($etiqueta) {
-					?>
-					<category term="<?=$etiqueta;?>" label="<?=$etiqueta;?>" />
-					<?
-				}
-			}
-			?>
-
-		</entry>
-
+		<content type="html">
+			<?=htmlspecialchars("<div><a href=\"".$url."\">".$titulo."</a></div>");?>
+			<? if ($desc) { ?>
+				<?=htmlspecialchars("<div>".$desc."</div>");?>
+			<? } ?>
+		</content>
+		<updated><?=$fecha;?></updated>
 		<?
-	}
+		foreach($etiquetas as $etiqueta) {
+			if ($etiqueta) {
+			?>
+			<category term="<?=$etiqueta;?>" label="<?=$etiqueta;?>" />
+			<?
+			}
+		}
+		?>
+	</entry>
+	<?
 }
+
 
 ?>
 
