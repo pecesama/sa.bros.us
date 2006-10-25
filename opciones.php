4268a4f5e68eeb1afbfbe71c057786eca50ae164
saboru <saboru@7d817b1d-ea13-0410-87bb-8e4317e5ca23>
2006-10-25 14:18:48 +0000

    Cambio de lugar de headers

--- opciones.php
+++ opciones.php
@@ -10,11 +10,12 @@
 
 
 ob_start();
+header("Content-type: text/html; charset=UTF-8");
 include("include/config.php");
 include("include/conex.php");
 include("include/functions.php");
 
-header("Content-type: text/html; charset=UTF-8");
+
 
 if (!esAdmin()) {
 	header("Location: login.php");
